a983c0ec2e9fad45e16983c0ce05205105a85be5
dutaposco <dutaposco@gmail.com>
2026-03-28 13:27:52 +0700

    Refactor subscription handling in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Programs from './pages/Programs'
-import Pricing from './pages/Pricing'
 import Coaches from './pages/Coaches'
 import VideoPlayer from './pages/VideoPlayer'
 
@@ -13,10 +12,10 @@ function App() {
   const [isSubscribed, setIsSubscribed] = useState(false)
   const [user, setUser] = useState(null)
 
-  const handleSubscribe = (plan) => {
+  const handleSubscribe = () => {
     // Simulate subscribing
     setIsSubscribed(true)
-    setUser({ name: 'Player', plan })
+    setUser({ name: 'Player' })
   }
 
   const handleLogout = () => {
@@ -26,12 +25,11 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar isSubscribed={isSubscribed} user={user} onLogout={handleLogout} />
+      <Navbar isSubscribed={isSubscribed} user={user} onLogout={handleLogout} onSubscribe={handleSubscribe} />
       <Routes>
-        <Route path="/" element={<Home isSubscribed={isSubscribed} />} />
-        <Route path="/programs" element={<Programs isSubscribed={isSubscribed} />} />
+        <Route path="/" element={<Home isSubscribed={isSubscribed} onSubscribe={handleSubscribe} />} />
+        <Route path="/programs" element={<Programs isSubscribed={isSubscribed} onSubscribe={handleSubscribe} />} />
         <Route path="/programs/:id" element={<VideoPlayer isSubscribed={isSubscribed} />} />
-        <Route path="/pricing" element={<Pricing isSubscribed={isSubscribed} onSubscribe={handleSubscribe} />} />
         <Route path="/coaches" element={<Coaches />} />
       </Routes>
       <Footer />
